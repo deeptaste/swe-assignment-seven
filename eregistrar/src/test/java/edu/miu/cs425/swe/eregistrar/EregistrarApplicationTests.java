38b673288acca52cf408e0eaf05f57c0fd2dcdc8
Diptesh Shrestha <dipteshshrestha@Dipteshs-MBP.attlocal.net>
2023-09-03 12:25:36 -0400

    Submission of SWE assignment 7

---
+++ eregistrar/src/test/java/edu/miu/cs425/swe/eregistrar/EregistrarApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs425.swe.eregistrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EregistrarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
